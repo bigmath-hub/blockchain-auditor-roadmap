9b2bce82dad303a3bb4f03573b3a47e18a2d78e3
bigmath <matheusdilly@live.com>
2026-01-01 19:42:38 +0300

    feat: criado contrato helloWorld para teste de ambiente

---
+++ 01-solidity-basics/helloWorld.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract HelloWorld {
+    // Uma variavel de estado para guardar texto
+    string public texto = "Hello Blockchain";
+
+    // Uma funcao para mudar o texto
+    fuction mudarTexto(string memory novoTexto) public {
+        texto = novoTexto;
+    }
+}
